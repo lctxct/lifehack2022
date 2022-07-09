88f63e86fe63112bf115e126b6a883d7f2e286f4
Tkaixiang <tkaixiang@gmail.com>
2022-07-09 17:55:39 +0800

    fixes

--- backend/controllers/listing/main.py
+++ backend/controllers/listing/main.py
@@ -6,8 +6,6 @@ model = SentenceTransformer('msmarco-distilbert-base-v4')
 
 searchQuery = sys.argv[1]
 query_embedding = model.encode(searchQuery)
-print(sys.argv[2])
-print(type(sys.argv[2]))
 corpus = json.loads(sys.argv[2])
 
 corpus_embeddings = model.encode(corpus, convert_to_tensor=True)
@@ -15,6 +13,7 @@ hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=5)
 hits = hits[0]      #Get the hits for the first query
 hitIDs = []
 for hit in hits:
-    hitIDs.append(hit['corpus_id'])
+    if (hit['score'] > 0.4): 
+        hitIDs.append(hit['corpus_id'])
 
 print(json.dumps(hitIDs))
