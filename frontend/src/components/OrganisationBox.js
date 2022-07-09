a01f516d75b353773f598c51ae1708658b3007a1
lctxct <caitinglien@hotmail.com>
2022-07-09 15:08:01 +0800

    Add search in home page

--- frontend/src/components/OrganisationBox.js
+++ frontend/src/components/OrganisationBox.js
@@ -1,26 +1,33 @@
 import react from "react";
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { Button, Grid, Paper, Typography } from "@mui/material/";
 
 const OrganisationBox = ({
   img,
   organisation,
   eventName,
-  timing, 
+  timing,
   location_name,
   ...props
 }) => {
-
   return (
     <Grid item xs={12} sm={4}>
       <Paper>
         <img src={img} alt="organisation" style={{ maxWidth: "100%" }} />
-        <Typography variant="h6">{eventName}</Typography>
-        <div>{organisation}</div>
-        <div><CalendarMonthIcon />{timing}</div>
-        <div><LocationOnIcon />{location_name}</div>
-        <Button variant="outlined">Find out more</Button>
+        <div style={{padding: '1vw'}}>
+          <Typography variant="h6">{eventName}</Typography>
+          <div>{organisation}</div>
+          <div>
+            <CalendarMonthIcon />
+            {timing}
+          </div>
+          <div>
+            <LocationOnIcon />
+            {location_name}
+          </div>
+          <Button variant="outlined">Find out more</Button>
+        </div>
       </Paper>
     </Grid>
   );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,40 +1,51 @@
-import { useEffect, useState} from 'react'; 
-import OrganisationBox from '../components/OrganisationBox'; 
-import { Grid } from '@mui/material';
-import defaultImg from '../test-data/image.jpg';
+import { useEffect, useState } from "react";
+import OrganisationBox from "../components/OrganisationBox";
+import { Grid, TextField } from "@mui/material";
+import defaultImg from "../test-data/image.jpg";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const DEFAULT_AUTH_TOKEN = process.env.REACT_APP_DEFAULT_AUTH_TOKEN;
 
 const Home = () => {
-    const [organisationData, setOrganisationData] =  useState([]); 
+  const [organisationData, setOrganisationData] = useState([]);
 
-    const getOrganisationData = async () => {
-        const response = await fetch(BACKEND_URL + '/display_volunteer_opportunity', {
-            method: 'GET', 
-            headers: {
-                'authorization': DEFAULT_AUTH_TOKEN,
-            }
-        })
+  const getOrganisationData = async () => {
+    const response = await fetch(
+      BACKEND_URL + "/display_volunteer_opportunity",
+      {
+        method: "GET",
+        headers: {
+          authorization: DEFAULT_AUTH_TOKEN,
+        },
+      }
+    );
 
-        const data = await response.json();
-        setOrganisationData(data.opportunities); 
-    }
+    const data = await response.json();
+    setOrganisationData(data.opportunities);
+  };
 
-    useEffect(() => {
-        getOrganisationData();
-    }, []);
-    
-    return (
-        <>
-         <Grid container spacing={3} style={{padding: '2vw'}}>
-            {organisationData.map(data => <OrganisationBox img={defaultImg} eventName={data.event_name} {...data} />)}
-        </Grid>
-        </>
-       
-    )
-}
+  useEffect(() => {
+    getOrganisationData();
+  }, []);
 
-export default Home; 
+  return (
+    <>
+      <div style={{fontWeight: 'bold'}}>
+        Describe the type of volunteering opportunity you're looking for!
+      </div>
+        <TextField placeholder="Start typing..." multiline rows={10} fullWidth style={{width: '40vw'}}/>
 
+      <Grid container spacing={3} style={{ padding: "2vw" }}>
+        {organisationData.map((data) => (
+          <OrganisationBox
+            img={defaultImg}
+            eventName={data.event_name}
+            {...data}
+          />
+        ))}
+      </Grid>
+    </>
+  );
+};
 
+export default Home;
