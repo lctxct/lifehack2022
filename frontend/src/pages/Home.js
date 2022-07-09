2eb06d3fb1e72968349598d2bf1efccf7ba7e3f9
Tkaixiang <tkaixiang@gmail.com>
2022-07-10 02:44:17 +0800

    home page css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import OrganisationBox from "../components/OrganisationBox";
+import DeleteIcon from '@mui/icons-material/Delete';
 import SearchIcon from '@mui/icons-material/Search';
 
 import { Button, CircularProgress, Grid, TextField, Paper } from "@mui/material";
@@ -12,6 +13,7 @@ const DEFAULT_AUTH_TOKEN = process.env.REACT_APP_DEFAULT_AUTH_TOKEN
 const Home = () => {
   const [organisationData, setOrganisationData] = useState([]);
   const [query, setQuery] = useState('');
+  const [currentlyFiltered, setCurrentlyFiltered] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
   const handleChange = (event) => {
@@ -35,45 +37,54 @@ const Home = () => {
         return res.json()
       }).then(data => {
         setOrganisationData(data.filteredOpportunities);
+        setCurrentlyFiltered(true)
         setIsLoading(false);
       });
     }
   }
 
+  const getOrganisationData = async () => {
+    const response = await fetch(
+      BACKEND_URL + "/display_volunteer_opportunity",
+      {
+        method: "GET",
+        headers: {
+          authorization: DEFAULT_AUTH_TOKEN,
+        },
+      }
+    );
+
+    const data = await response.json();
+    setOrganisationData(data.opportunities);
+    setIsLoading(false);
+  };
+
+  const handleClearFilter = () => {
+    setIsLoading(true);
+    setQuery("")
+    setCurrentlyFiltered(false)
+    getOrganisationData()
+  }
+
   useEffect(() => {
-    const getOrganisationData = async () => {
-      const response = await fetch(
-        BACKEND_URL + "/display_volunteer_opportunity",
-        {
-          method: "GET",
-          headers: {
-            authorization: DEFAULT_AUTH_TOKEN,
-          },
-        }
-      );
-
-      const data = await response.json();
-      setOrganisationData(data.opportunities);
-      setIsLoading(false);
-    };
-
     getOrganisationData();
   }, []);
 
   return (
     <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
       <div style={{ margin: '2vw' }}>
-        <Paper elevation={20} style={{ padding: "5ch", borderRadius: "30px" }}>
+        <Paper elevation={20} style={{ padding: "5ch", borderRadius: "30px", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
           <div style={{ fontWeight: "bold", fontSize: "1.5vw" }}>
             Describe the type of volunteering opportunity you're looking for!
           </div>
 
-          <form onSubmit={onSubmit} style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+          <form onSubmit={onSubmit} style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
             <TextField
               placeholder="E.g I love nature and would love to help with guiding others to learn more about Singapore's nature reserves."
               multiline
               rows={5}
               fullWidth
+              value={query}
               style={{ width: "40vw", margin: '2vw' }}
               onChange={handleChange}
             />
@@ -81,6 +92,12 @@ const Home = () => {
               <Button variant="contained" type="submit" sx={{ fontFamily: 'inherit' }} endIcon={<SearchIcon />}>Get Recommendations</Button>
             </div>
           </form>
+
+          {currentlyFiltered && (
+              <div>
+                <Button variant="contained" sx={{ fontFamily: 'inherit', marginTop: "3ch" }} color="error" endIcon={<DeleteIcon />} onClick={() => {handleClearFilter()}}>Clear Filter</Button>
+              </div>
+            )}
         </Paper>
       </div>
       <div>
