a47a1ef178a7b838c3b6387046baf015e513f93a
lctxct <caitinglien@hotmail.com>
2022-07-09 14:19:08 +0800

    Add fetch organisation data

--- frontend/src/components/OrganisationBox.js
+++ frontend/src/components/OrganisationBox.js
@@ -1,10 +1,27 @@
 import react from "react";
-import { Grid, Paper } from "@mui/material/";
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { Button, Grid, Paper, Typography } from "@mui/material/";
 
-const OrganisationBox = ({ title }) => {
+const OrganisationBox = ({
+  img,
+  organisation,
+  eventName,
+  timing, 
+  location_name,
+  ...props
+}) => {
+
   return (
     <Grid item xs={12} sm={4}>
-      <Paper>{title}</Paper>
+      <Paper>
+        <img src={img} alt="organisation" style={{ maxWidth: "100%" }} />
+        <Typography variant="h6">{eventName}</Typography>
+        <div>{organisation}</div>
+        <div><CalendarMonthIcon />{timing}</div>
+        <div><LocationOnIcon />{location_name}</div>
+        <Button variant="outlined">Find out more</Button>
+      </Paper>
     </Grid>
   );
 };
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,37 @@
-import react from 'react'; 
+import { useEffect, useState} from 'react'; 
 import OrganisationBox from '../components/OrganisationBox'; 
 import { Grid } from '@mui/material';
+import defaultImg from '../test-data/image.jpg';
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const DEFAULT_AUTH_TOKEN = process.env.REACT_APP_DEFAULT_AUTH_TOKEN;
 
 const Home = () => {
+    const [organisationData, setOrganisationData] =  useState([]); 
 
-    const listItems = [1, 2, 3]; 
+    const getOrganisationData = async () => {
+        const response = await fetch(BACKEND_URL + '/display_volunteer_opportunity', {
+            method: 'GET', 
+            headers: {
+                'authorization': DEFAULT_AUTH_TOKEN,
+            }
+        })
+
+        const data = await response.json();
+        setOrganisationData(data.opportunities); 
+    }
+
+    useEffect(() => {
+        getOrganisationData();
+    }, []);
+    
     return (
-        <Grid container spacing={2}>
-            {listItems.map(item => <OrganisationBox title={item} />)}
+        <>
+         <Grid container spacing={3} style={{padding: '2vw'}}>
+            {organisationData.map(data => <OrganisationBox img={defaultImg} eventName={data.event_name} {...data} />)}
         </Grid>
+        </>
+       
     )
 }
 
