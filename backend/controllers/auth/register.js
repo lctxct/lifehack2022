e7fd9c480bd995aebbe3e713b71ca5cab6b61e3d
Tkaixiang <tkaixiang@gmail.com>
2022-07-09 16:22:53 +0800

    bio

--- backend/controllers/auth/register.js
+++ backend/controllers/auth/register.js
@@ -3,7 +3,7 @@ const authentication = require("../../authentication.js")
 const argon2 = require('argon2')
 
 const register = async (req, res) => {
-    const paramsNeeded = ["username", "password", "age", "location", "categories", "timings", "telegram_id"]
+    const paramsNeeded = ["username", "password", "age", "location", "categories", "timings", "telegram_id", "bio"]
 
     for (let i = 0; i < paramsNeeded.length; i++) {
         if (!(paramsNeeded[i] in req.body)) return res.send({
@@ -22,7 +22,8 @@ const register = async (req, res) => {
         location: req.body.location.toLowerCase(),
         categories: cleanedCategories,
         timings: req.body.timings,
-        telegram_id: req.body.telegram_id
+        telegram_id: req.body.telegram_id,
+        bio: req.body.bio
     })
 
     const signer = authentication.getSigner()
