4713bddf33b59b0dd62d417a341732f0434a7ae6
Tkaixiang <tkaixiang@gmail.com>
2022-07-10 01:46:37 +0800

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,53 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import Home from './pages/Home'; 
-import Buddying from './pages/Buddying'; 
-import NotFound from './pages/NotFound'; 
-import { useState } from 'react'; 
+import Home from './pages/Home';
+import Buddying from './pages/Buddying';
+import Login from './pages/Login'
+import NotFound from './pages/NotFound';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState(0); 
+  const [currentPage, setCurrentPage] = useState(0);
+  const [token, setToken] = useState(false)
+  const [username, setUsername] = useState("John Doe")
+
+  useEffect(() => {
+    const localStorageToken = localStorage.getItem("auth-token")
+    if (localStorageToken !== null) {
+      window.token = localStorageToken
+      setToken(localStorageToken)
+    }
+  })
+
+  const handleNewLogin = (token, rememberMe) => {
+    if (rememberMe) {
+      localStorage.setItem("auth-token", token)
+    }
+    setToken(token)
+    setUsername(token.split(".")[0])
+  }
 
   const setPage = page => {
-    console.log(page); 
-    setCurrentPage(page); 
+    console.log(page);
+    setCurrentPage(page);
   }
 
   return (
     <div className="App">
-      <NavBar setPage={setPage}/> 
-      {currentPage === 0 && <Home />}
-      {currentPage === 1 && <Buddying />}
-      {currentPage === 2 && <NotFound />}
-      {currentPage > 2 && <NotFound />}
-    
-    </div>
+      {token ? (
+        <div>
+          <NavBar setPage={setPage} />
+          {currentPage === 0 && <Home />}
+          {currentPage === 1 && <Buddying />}
+          {currentPage === 2 && <NotFound />}
+          {currentPage > 2 && <NotFound />}
+        </div>
+      ) : (
+        <Login handleNewLogin={handleNewLogin}/>
+      )
+      }
+
+    </div >
   );
 }
 
