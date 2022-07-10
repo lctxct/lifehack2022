fb57ed80c78ee299f34b674693af3c359d24be6a
leemzius <leemah.bisht@gmail.com>
2022-07-10 09:08:57 +0800

    org page

--- frontend/src/components/OrganisationEvent.js
+++ frontend/src/components/OrganisationEvent.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
+import { Button, Grid, Paper, Typography, Card}  from "@mui/material";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
@@ -8,10 +8,12 @@ import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import InfoIcon from '@mui/icons-material/Info';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -24,46 +26,124 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function EventCard(props) {
+// export default function EventCard(props) {
+//   const [expanded, setExpanded] = React.useState(false);
+
+//   const handleExpandClick = () => {
+//     setExpanded(!expanded);
+//   };
+
+//   return (
+//     <Card sx={{ maxWidth: 345 }}>
+//       <CardHeader title={props.title} subheader={props.subheader} />
+//       <CardMedia component="img" height="194" image={props.img} alt="test" />
+//       <CardContent>
+//         <Typography variant="body2" color="text.secondary">
+//           What can you expect to learn?
+//         </Typography>
+//         <Typography variant="body2" color="text.secondary">
+//           {props.takeaways}
+//         </Typography>
+//       </CardContent>
+//       <CardActions disableSpacing>
+//         <IconButton aria-label="add to favorites">
+//           <FavoriteIcon />
+//         </IconButton>
+//         <IconButton aria-label="share">
+//           <ShareIcon />
+//         </IconButton>
+//         <ExpandMore
+//           expand={expanded}
+//           onClick={handleExpandClick}
+//           aria-expanded={expanded}
+//           aria-label="show more"
+//         >
+//           <ExpandMoreIcon />
+//         </ExpandMore>
+//       </CardActions>
+//       <Collapse in={expanded} timeout="auto" unmountOnExit>
+//         <CardContent>
+//           <Typography paragraph>{props.desc}</Typography>
+//         </CardContent>
+//       </Collapse>
+//     </Card>
+//   );
+// }
+
+
+
+
+
+
+
+
+const OrganisationCardV2 = ({
+  img,
+  organisation,
+  eventName,
+  timing,
+  location_name,
+  handleClick,
+  description,
+  training_program,
+
+  ...props
+}) => {
   const [expanded, setExpanded] = React.useState(false);
-
   const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
+        setExpanded(!expanded);
+      };
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader title={props.title} subheader={props.subheader} />
-      <CardMedia component="img" height="194" image={props.img} alt="test" />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          What can you expect to learn?
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {props.takeaways}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
+    <Grid item xs={12} sm={4}>
+      <Paper>
+        <img src={img} alt="organisation" style={{ maxWidth: "100%",  }} />
+        <div style={{padding: '1vw'}}>
+          <Typography variant="h5">{eventName}</Typography>
+          <div style={{textTransform: "capitalize", marginTop: "2ch", fontWeight: "bold"}}>{organisation}</div>
+          <div style={{marginTop: "0.5ch", display: "flex", alignItems: "center"}}>
+            <CalendarMonthIcon />
+            {timing}
+          </div>
+          <div style={{marginTop: "0.5ch", textTransform: "capitalize", display: "flex", alignItems: "center"}}>
+            <LocationOnIcon />
+            {location_name}
+          </div>
+          <div style={{marginTop: "0.5ch", textTransform: "capitalize", display: "flex", alignItems: "center"}}>
+            <Typography variant="body2" color="text.secondary">
+              What can you expect to learn?
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {training_program}
+            </Typography>
+          </div>
+          <div style={{marginTop: "0.5ch", textTransform: "capitalize", display: "flex", alignItems: "center"}}>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          < IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
           aria-expanded={expanded}
           aria-label="show more"
         >
           <ExpandMoreIcon />
+          
         </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>{props.desc}</Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+        </div>
+        
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>{description}</Typography>
+            </CardContent>
+          </Collapse>        
+      
+      </div>
+      </Paper>
+    </Grid>
   );
-}
+};
+
+export default OrganisationCardV2;
--- frontend/src/pages/organisationPage.js
+++ frontend/src/pages/organisationPage.js
@@ -1,11 +1,84 @@
 import { useEffect, useState } from "react";
-import  EventCard from "../components/OrganisationEvent";
+
+import  OrganisationCardV2 from "../components/OrganisationEvent";
 import { Grid, TextField } from "@mui/material";
 import defaultImg from "../test-data/image.jpg";
 import Stack from '@mui/material/Stack';
 import OrgProfile from  '../components/OrganisationProfile';
 
-export default function OrganisationPage() {
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const DEFAULT_AUTH_TOKEN = process.env.REACT_APP_DEFAULT_AUTH_TOKEN
+
+const OrganisationPage = () => {
+    const [organisationData, setOrganisationData] = useState([]);
+    const [query, setQuery] = useState('');
+    const [currentlyFiltered, setCurrentlyFiltered] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [ modal, setModal ] = useState(true); 
+  
+    const handleChange = (event) => {
+      setQuery(event.target.value);
+    };
+  
+    const handleClick = () =>  {
+      setModal(!modal); 
+    }
+  
+    const onSubmit = (event) => {
+      event.preventDefault();
+      if (query.length > 0) {
+        setIsLoading(true);
+        fetch(BACKEND_URL + '/search_volunteer_opportunity', {
+          // how can i specify which org i want to take the data from? 
+          method: 'post',
+          headers: {
+            authorization: DEFAULT_AUTH_TOKEN,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            "query": query
+          })
+        }).then(res => {
+          return res.json()
+        }).then(data => {
+          opps = data.filteredOpportunities.filter(r => r.organisation == "the organisation u want");
+          setOrganisationData(opps);
+        
+
+          setCurrentlyFiltered(true)
+          setIsLoading(false);
+        });
+      }
+    }
+  
+    const getOrganisationData = async () => {
+      const response = await fetch(
+        BACKEND_URL + "/display_volunteer_opportunity",
+        {
+          method: "GET",
+          headers: {
+            authorization: DEFAULT_AUTH_TOKEN,
+          },
+        }
+      );
+  
+      const data = await response.json();
+      setOrganisationData(data.opportunities);
+      setIsLoading(false);
+    };
+
+  
+    const handleClearFilter = () => {
+      setIsLoading(true);
+      setQuery("")
+      setCurrentlyFiltered(false)
+      getOrganisationData()
+    }
+  
+    useEffect(() => {
+      getOrganisationData();
+    }, []);
   
   return (
     <Stack direction="row" alignItems="stretch" spacing={0}>
@@ -14,31 +87,21 @@ export default function OrganisationPage() {
       </item>
 
       <item>
-        <Grid container spacing={3} style={{ padding: "2vw" }}>
-          <EventCard
-            title="test1"
-            subheader="test1"
-            takeaways="test1"
-            desc="test1"
-            img={defaultImg}
-          />
+         <Grid container spacing={3} style={{ padding: "2vw" }}>
 
-          <EventCard
-            title="test1"
-            subheader="test1"
-            takeaways="test1"
-            desc="test1"
-            img={defaultImg}
-          />
-          <EventCard
-            title="test1"
-            subheader="test1"
-            takeaways="test1"
-            desc="test1"
-            img={defaultImg}
-          />
+            {organisationData.map((data) => (
+              <OrganisationCardV2
+                img={defaultImg}
+                eventName={data.event_name}
+                // handleClick={handleClick}
+                {...data}
+                sx={{ fontFamily: 'inherit' }}
+              />
+            ))}
         </Grid>
       </item>
     </Stack>
   );
 }
+
+export default OrganisationPage 
