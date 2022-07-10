95d0c634a17038a2b4f2dbcfe6732a87cc392cf7
Newst Fillantrophy <fillantrophy@gmail.com>
2022-07-10 09:46:58 +0800

    edit css

--- frontend/src/components/BuddyingFilterBar.js
+++ frontend/src/components/BuddyingFilterBar.js
@@ -2,6 +2,16 @@ import { categories } from "../types/Organisations";
 import { useState } from "react";
 import { TextField } from "@mui/material";
 import MultiSelect from "./MultiSelect";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const customTheme = createTheme({
+  palette: {
+    light: {main: '#CDBCA5', contrastText: '#FFFFFF'},
+    mediumlight: {main: '#d4c4b4', contrastText: '#FFFFFF'},
+    neutral: {main: '#98745C', contrastText: '#FFFFFF'},
+    dark: {main: '#060606', contrastText: '#FFFFFF'},
+  },
+});
 
 const BuddyingFilterBar = ({
   handleCategoryFilterChange,
@@ -9,7 +19,8 @@ const BuddyingFilterBar = ({
   categoryFilter,
   locationFilter,
 }) => {
-  const locations = ["dummyData!"];
+  const locations = ["Aljunied", "Hougang", "Ang Mo Kio", "Kebun Baru", "Yio Chu Kang", "Bishan–Toa Payoh", "Marymount", "Chua Chu Kang", "Hong Kah North", "East Coast", "Holland–Bukit Timah", "Bukit Panjang", "Jalan Besar", "Potong Pasir", "Jurong", "Bukit Batok", "Yuhua", "Marine Parade", "MacPherson", "Mountbatten", "Marsiling–Yew Tee", "Nee Soon", "Pasir Ris–Punggol", "Punggol West", "Sembawang", "SSengkang", "Tampines", "Tanjong Pagar", "Radin Mas", "West Coast", "Pioneer"
+]
 
   return (
     <div>
@@ -21,6 +32,7 @@ const BuddyingFilterBar = ({
       /> */}
 
       <MultiSelect
+        sx={{fontFamily:"inherit"}}
         label="Categories"
         items={categories}
         itemFilter={categoryFilter}
